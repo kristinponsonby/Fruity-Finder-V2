b02ff86229547315f5acce14c23a321bcbc91876
Kristin Ponsonby <kristinponsonby@gmail.com>
2022-01-29 09:10:29 -0500

    Create README file

---
+++ backend/README.md
@@ -0,0 +1,10 @@
+Welcome to Fruity Finder!
+
+To get this app up and running you will need to run the following commands:
+
+npm install express
+
+npm install request 
+
+run the server by running:
+node app.js
--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+var request = require("request");
 const app = express();
-const port = 3000
+const port = 3001
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-}) 
+app.get('/', (req, res) => {res.send('Hello World');}) 
+app.get('/fruits', (req, res) => {
+    request('https://fruityvice.com/api/fruit/all', function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            res.send(body);
+        }
+    })
+});
 
-app.listen(3000,() => {
- console.log("starting...");
+app.listen(port, () => {
+ console.log(`starting on ${port}`);
 });
